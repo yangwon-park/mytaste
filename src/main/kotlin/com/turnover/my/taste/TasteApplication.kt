3990b03479cdd068c7eb9388439e2fcde3ad30e6
Yangwon Park <ywonp94@gmail.com>
2024-01-16 11:12:48 +0900

    :tada: "밥묵다(가명) 프로젝트 시작"

---
+++ src/main/kotlin/com/turnover/my/taste/TasteApplication.kt
@@ -0,0 +1,11 @@
+package com.turnover.my.taste
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TasteApplication
+
+fun main(args: Array<String>) {
+	runApplication<TasteApplication>(*args)
+}
---
+++ src/test/kotlin/com/turnover/my/taste/TasteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.turnover.my.taste
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TasteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
