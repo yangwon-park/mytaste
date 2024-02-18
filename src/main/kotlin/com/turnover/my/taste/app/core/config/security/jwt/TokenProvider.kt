f47e3b046d33a45ceb136210dca14cba726cd153
work <ywonp94@gmail.com>
2024-02-18 15:21:48 +0900

    :necktie: "JWT 유효 시간 및 리프래쉬 시간 설정"

--- src/main/kotlin/com/turnover/my/taste/app/core/config/security/jwt/TokenProvider.kt
+++ src/main/kotlin/com/turnover/my/taste/app/core/config/security/jwt/TokenProvider.kt
@@ -2,6 +2,7 @@ package com.turnover.my.taste.app.core.config.security.jwt
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 private val logger = KotlinLogging.logger {}
@@ -9,8 +10,16 @@ private val logger = KotlinLogging.logger {}
 
 @Component
 class TokenProvider(
-    private val jwtConfig: JwtConfig
+    private val jwtConfig: JwtConfig,
+
+    @Value("\${jwt.token-validity-in-sec}")
+    private val JWT_TOKEN_VALIDITY: String,
+
+    @Value("\${jwt.refresh-token-validity-in-sec}")
+    private val REFRESH_TOKEN_VALIDITY: Long,
 ): InitializingBean {
+
+
 
     // Bean 생성 후 secret 값을 Base64 Decode하여 변수에 할당하기 위함
     override fun afterPropertiesSet() {
